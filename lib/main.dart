0f3e790e7efca56c445546aad6d4a1079296f274
Yuya-Ito325 <c1004072@planet.kanazawa-it.ac.jp>
2020-12-16 18:40:38 +0900

    漫画を一覧表示する画面で漫画を表示するリストを追加

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var list = ["漫画１","漫画２","漫画３","漫画４",];
     return MaterialApp(
       title: 'Find Comics',
       theme: ThemeData(
@@ -16,18 +17,31 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: Column(
         children: <Widget>[
-          Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
+          // 漫画の情報を表示するリスト
+          Container(
+            height: 125,
+            padding: EdgeInsets.all(4),
+            // childrenを指定してリスト表示
+            child: ListView.builder(
+              itemBuilder: (BuildContext context, int index) {
+                return Text(list [index]);
+              },
+              itemCount: list.length,
+            ),
+          ),
+
 
             // カメラを起動するボタン
             FlatButton(
               onPressed: () {},
               child: Text('Camera'),
             ),
-
-          ],
-        ),]),
+            // Raw{
+            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            // children: <Widget>[
+            //
+            //     }
+        ]),
       )
     );
   }
