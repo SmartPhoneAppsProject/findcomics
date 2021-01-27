99e362b36609c4ee2ec43d5ccb71137059bbc8b6
Shinbo-Rina <b1915499@planet.kanazawa-it.ac.jp>
2021-01-27 18:13:13 +0900

    漫画を追加する画面にテキストフィールドを作成

--- lib/rename.dart
+++ lib/rename.dart
@@ -2,20 +2,38 @@ import 'package:flutter/material.dart';
 
 class Rename extends StatelessWidget {
 
-  final String name;
+  TextEditingController _controller = new TextEditingController();
+  String name;
 
   Rename({Key key, @required this.name}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
 
+    _controller.text = name;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('本を追加する'),
       ),
       body: Center(
-        child: Text(name),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextFormField(
+              controller: _controller,
+              decoration: const InputDecoration(
+                border: const UnderlineInputBorder(),
+                hintText: 'テキストを入力してください',
+                labelText: 'タイトル',
+              ),
+            ),
+          ]
+        ),
       ),
+      // body: Center(
+      //   child: Text(name),
+      // ),
     );
   }
 }
